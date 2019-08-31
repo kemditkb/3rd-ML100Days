d9b0c7ebcda749f13827648a7ae487aab9e8aa6a
escc1122 <escc1122@gmail.com>
2019-08-31 13:48:43 +0800

    test

--- D1/example.py
+++ D1/example.py
@@ -9,39 +9,43 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Python 的變數不須宣告, 可能是文字, 數值, 陣列, 甚至是物件, 對初學者來說這往往是最難接受的地方
-# 主要是 Python 在大多數情況下, 可以由運算"猜"出你想要的型態, 我們由下列語法看看發生了什麼事吧
-# w, b 是數值
-w = 3
-b = 0.5
+def main():
+    int_w = 3;
+    double_b = 0.5;
+    # np.linspace 是 numpy.linspace 的意思
+    # np.linspace(0, 100, 101)是指 0~100 劃分成 101 個刻度(含頭尾), 所也就是 0, 1, 2,...,100 這 101 個數
+    # 這時候, x_lin 因為要記錄不只一個數, 因為 np.linspace() 傳回的是一個 Array, 所以 x_lin 就變成 Array 了
+    array_x_lin = np.linspace(0, 100, 101);
+    # np.random.randn() 就是 numpy.random.randn(), 會隨機傳回標準常態分布的取樣值
+    # np.random.randn(101) 表示取樣了101次, 型態是 Array, 所以其他 + 與 * 的部分都是 Array 的加與乘, 一行就計算了101筆資料
+    # 所以最後的結果 y, 也是一個長度 101 的 Array
+    array_y = (array_x_lin + np.random.randn(101) * 5) * int_w + double_b;
+    add_point(array_x_lin,array_y);
+    plt.show();
+    
+    y_hat = array_x_lin * int_w + double_b;
+    add_point(array_x_lin,array_y);
+    add_line(array_x_lin,y_hat)
+    plt.show();
+    
+    # 呼叫上述函式, 傳回 y(藍點高度)與 y_hat(紅線高度) 的 MAE
+    MAE = mean_absolute_error(array_y, y_hat);
+    print("The Mean absolute error is %.3f" % (MAE));
+    
+    mean_square_error(array_y, y_hat);
+    
+def add_point(array_x_lin,array_y):
+    # b. : b 就是 blue, 點(.) 就是最小單位的形狀, 詳細可以查 matplotlib 的官方說明
+    plt.plot(array_x_lin, array_y, 'b.', label = 'data points')
+    plt.title("Assume we have data points")
+    plt.legend(loc = 2);
 
-# np.linspace 是 numpy.linspace 的意思
-# np.linspace(0, 100, 101)是指 0~100 劃分成 101 個刻度(含頭尾), 所也就是 0, 1, 2,...,100 這 101 個數
-# 這時候, x_lin 因為要記錄不只一個數, 因為 np.linspace() 傳回的是一個 Array, 所以 x_lin 就變成 Array 了
-x_lin = np.linspace(0, 100, 101)
-
-# np.random.randn() 就是 numpy.random.randn(), 會隨機傳回標準常態分布的取樣值
-# np.random.randn(101) 表示取樣了101次, 型態是 Array, 所以其他 + 與 * 的部分都是 Array 的加與乘, 一行就計算了101筆資料
-# 所以最後的結果 y, 也是一個長度 101 的 Array
-y = (x_lin + np.random.randn(101) * 5) * w + b
-
-# 這邊就是將 x_lin 以及剛剛算完的 y, 當作座標值, 將101個點在平面上畫出來
-# b. : b 就是 blue, 點(.) 就是最小單位的形狀, 詳細可以查 matplotlib 的官方說明
-plt.plot(x_lin, y, 'b.', label = 'data points')
-plt.title("Assume we have data points")
-plt.legend(loc = 2)
-plt.show()
-
-# 這邊的 y_hat, 就沒有隨機的部分了, 也就是下圖中的紅色實線部分
-y_hat = x_lin * w + b
-plt.plot(x_lin, y, 'b.', label = 'data')
-# 上面的 'b.' 是藍色點狀, 下面的 'r-' 是紅色線狀, label 是圖示上的名稱
-plt.plot(x_lin, y_hat, 'r-', label = 'prediction')
-plt.title("Assume we have data points (And the prediction)")
-plt.legend(loc = 2)
-plt.show()
-
-
+def add_line(array_x_lin,y_hat):
+    # 上面的 'b.' 是藍色點狀, 下面的 'r-' 是紅色線狀, label 是圖示上的名稱
+    plt.plot(array_x_lin, y_hat, 'r-', label = 'prediction')
+    plt.title("Assume we have data points (And the prediction)")
+    plt.legend(loc = 2);
+    
 # Python 的函數是另一個新手上手的困難點, 由def開頭, 依序是函數名稱 / 輸入值, 冒號(:)結尾
 # 最難讓人習慣的是 Python 的函式與條件判斷, 前後都沒有大括弧(其他程式常見), 而是以四格空白縮排來取代
 # 以本例來說, mean_absolute_error 這個函數的定義範圍到 return mae 為止, 因為中間都是縮排, 而 """ 是多行註解(井號是單行註解)
@@ -59,11 +63,6 @@ def mean_absolute_error(y, yp):
     mae = sum(abs(y - yp)) / len(y)
     return mae
 
-# 呼叫上述函式, 傳回 y(藍點高度)與 y_hat(紅線高度) 的 MAE
-MAE = mean_absolute_error(y, y_hat)
-print("The Mean absolute error is %.3f" % (MAE))
-
-
 # https://zhuanlan.zhihu.com/p/37663120
 #
 def mean_square_error(y, yp):
@@ -73,6 +72,9 @@ def mean_square_error(y, yp):
     
     var_sk_mse = mean_squared_error(y,yp)
     print("The Mean square error sk learn is %.3f" % var_sk_mse)
+
     
-mean_square_error(y, y_hat);
+    
+    
+main();
 
