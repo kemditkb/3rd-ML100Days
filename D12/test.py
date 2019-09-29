6c05f052d3dcbd9638e3b1aa0d6d894e00177e4a
escc1122 <escc1122@gmail.com>
2019-09-29 17:59:34 +0800

    d12

---
+++ D12/test.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 29 16:17:40 2019
+
+@author: escc
+"""
+
+# 做完特徵工程前的所有準備 (與前範例相同)
+import pandas as pd
+import numpy as np
+import copy
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import LogisticRegression
+
+data_path = '../csv/'
+df_train = pd.read_csv(data_path + 'titanic_train.csv')
+df_test = pd.read_csv(data_path + 'titanic_test.csv')
+
+train_Y = df_train['Survived']
+ids = df_test['PassengerId']
+df_train = df_train.drop(['PassengerId', 'Survived'] , axis=1)
+df_test = df_test.drop(['PassengerId'] , axis=1)
+df = pd.concat([df_train,df_test])
+df.head()
+
+#只取 int64, float64 兩種數值型欄位, 存於 num_features 中
+num_features = []
+for dtype, feature in zip(df.dtypes, df.columns):
+    if dtype == 'float64' or dtype == 'int64':
+        num_features.append(feature)
+print(f'{len(num_features)} Numeric Features : {num_features}\n')
+
+# 削減文字型欄位, 只剩數值型欄位
+df = df[num_features]
+train_num = train_Y.shape[0]
+df.head()
+
+
+
+# 空值補 -1, 做羅吉斯迴歸
+df_m1 = df.fillna(-1)
+train_X = df_m1[:train_num]
+estimator = LogisticRegression()
+cross_val_score(estimator, train_X, train_Y, cv=5).mean()
+
+df_m0 = df.fillna(0)
+train_X = df_m0[:train_num]
+estimator = LogisticRegression()
+cross_val_score(estimator, train_X, train_Y, cv=5).mean()
+
+df_mn = df.fillna(df.mean())
+train_X = df_mn[:train_num]
+estimator = LogisticRegression()
+cross_val_score(estimator, train_X, train_Y, cv=5).mean()
